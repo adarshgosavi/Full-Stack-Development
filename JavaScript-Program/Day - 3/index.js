0b8b5a3f23db4679072b3444154a60b2ea8740b1
adarshgosavi <aadrshgosavi89@gmail.com>
2026-08-02 21:14:26 +0530

    Your commit message

---
+++ JavaScript-Program/Day - 3/index.js
@@ -0,0 +1,28 @@
+// Arithmetic Operator
+// console.log(2+5);
+// console.log(2-5);
+// console.log(2*5);
+// console.log(2/5);
+// console.log(2%5);
+// console.log(2**5);
+// console.log("------------------------------------")
+
+
+// // Assignment Operator
+// let x = 20;
+// let y = 10;
+// // x = x+y;
+// x -= y;
+// console.log(x)
+// console.log("------------------------------------")
+
+
+// Comparison Operator
+// let x = 10;
+// let y = 20;
+// console.log(x===y);
+
+
+let a = "10";
+let b = Number(a);
+console.log(typeof b)
