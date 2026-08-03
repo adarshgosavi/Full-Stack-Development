d33f1284540864b84d17116ce8e16f9079a2fa3b
adarshgosavi <aadrshgosavi89@gmail.com>
2026-08-03 17:01:11 +0530

    Your commit message

--- JavaScript-Program/Day - 3/index.js
+++ JavaScript-Program/Day - 3/index.js
@@ -18,11 +18,121 @@
 
 
 // Comparison Operator
-// let x = 10;
-// let y = 20;
-// console.log(x===y);
+// let x = 20;
+// let y = "20";
+// console.log(x>y);
+// console.log(x>=y);
+// console.log(x<y);
+// console.log(x<=y);
+// console.log(x==y); //number and string compare(string convert number)
+// console.log(x===y); // type check(dono same type ke hone chaiye ===, uske baad wo comparison)
 
 
-let a = "10";
-let b = Number(a);
-console.log(typeof b)
+
+// String -> Number
+// let a = "10";
+// let b = Number(a);
+// console.log(typeof b)
+
+
+// let a = "121ab"
+// let b = Number(a);
+// console.log(typeof b)
+
+
+// console.log(Number(true));
+// console.log(Number(false));
+
+
+// console.log(Number(null));
+// console.log(Number(undefined));
+
+// console.log(Boolean(true))
+
+//Number -> String
+// let a1 = 10;
+// let b1 = String(a1);
+// console.log(typeof b1)
+
+
+
+// computer science challenge
+// let a = 0.1;
+// let b = 0.2;
+// let c = a + b;
+// console.log(c)
+
+
+// 1: null is loosly equal to undefined only
+// console.log(null == undefined)
+// console.log(null === undefined)
+// console.log(null == 0)
+
+
+
+// > , < , <= , >= (null -> number , undefined --> NaN)
+// console.log(null >= 0) 
+// console.log(null <= 0)
+// console.log(null > 0)
+// console.log(null < 0)
+
+// console.log(null >= undefined)
+// console.log(undefined >= 0)
+
+// console.log("Rohit" > "aaa")
+
+
+
+// for Loop 
+// for(let i = 1; i<=10; ++i){
+//     console.log(i)
+// }
+
+//while loop
+// let i = 1;
+// while(i <= 10){
+//     console.log(i)
+//     i++
+// }
+
+// do-while loop
+// let i = 1;
+// do{
+//     console.log(i)
+//     i++
+// }while(i<=10)
+
+
+// if else statement
+
+// let age = 15
+// if(age >= 18){
+//     console.log("Eligible for vote")
+// }else{
+//     console.log("Not Eligible for vote")
+// }
+
+
+//example: kid , young and old
+
+// let age = 12;
+// if(age <= 18){
+//     console.log("Kid")
+// }else if(age >= 60){
+//     console.log("old")
+// }else{
+//     console.log("young")
+// }
+
+
+//Logical Operator:- && , ||
+let a = 10;
+let b = 10;
+let c = 20;
+console.log(Boolean(a && b))
+console.log(Boolean(a && c))
+console.log(Boolean(a || b))
+console.log(Boolean(a || c))
+
+// != 
+console.log( 5 != 5)
