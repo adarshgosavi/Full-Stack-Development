244a32d87d90b49cff4f915c6902431a66df8fcb
adarshgosavi <aadrshgosavi89@gmail.com>
2026-08-03 21:30:00 +0530

    Your commit message

---
+++ JavaScript-Program/Day - 4/index.js
@@ -0,0 +1,32 @@
+//  let a = 102.5464;
+//  let b = 457.5478;
+//  console.log(a.toFixed(10))
+
+// console.log(b.toString())
+
+// let a = new Number(20)
+// let b = new Number(20)
+// console.log(a == b)
+
+// let obj1 = {
+//     name : "Adarsh",
+//     age : 21,
+//     class : "BCA"
+// }
+// let obj2 = obj1;
+
+// console.log(obj1 == obj2)
+
+
+// let a = 10;
+// let b = a;
+// console.log(a==b)
+
+
+
+// console.log(Math.abs(-3))
+// console.log(Math.PI)
+// console.log(Math.LN10)
+// console.log(Math.max(10,54,87,875,85,75,84,58,445,87))
+// console.log(Math.min(4,57,47,8,51,2,85,5))
+console.log(Math.floor (Math.random()*10))
