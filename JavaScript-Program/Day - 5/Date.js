37ecebad70c20e204b4b76f39077b7440473c50d
adarshgosavi <aadrshgosavi89@gmail.com>
2026-08-05 13:18:48 +0530

    Your commit message

---
+++ JavaScript-Program/Day - 5/Date.js
@@ -0,0 +1,28 @@
+// const now = new Date();
+// console.log(now)
+// console.log(now.toString())
+// console.log(now.toISOString())
+// console.log(now.toLocaleString())
+
+// console.log(now.getDate())
+// console.log(now.getMonth())
+// console.log(now.getFullYear())
+// console.log(now.getHours())
+// console.log(now.getMinutes())
+// console.log(now.getSeconds())
+
+
+//Days: Mon-Tue(1 based)
+//Month: 0 1
+
+// Year -> Month -> Date -> Hour -> Minutes -> Seacond -> Millisecond
+// const now = new Date(2026,7,5,12,55,12);
+// console.log(now.toString())
+
+
+
+const now = Date.now()
+const date = new Date(-472544544512)
+// console.log(date.toString())
+console.log(date)
+console.log(now)
---
+++ JavaScript-Program/Day - 5/index.js
@@ -0,0 +1,60 @@
+// String
+// const str1 = "Adarsh";
+// const str2 = 'Adarsh';
+// const day = 18;
+// const str3 = `Strike is coming on ${day}`;
+// console.log(str3)
+
+
+// const str = `Hello I'm Adarsh Gosavi...!`;
+// console.log(str.length);
+// console.log(str[6]);
+
+// const a = str.toUpperCase()
+// const b = str.toLowerCase()
+// console.log(a)
+// console.log(b)
+
+
+
+// const str = `Hello I'm Adarsh Gosavi...!`;
+
+// console.log(str.indexOf('savi'))
+// console.log(str.lastIndexOf('rshc'))
+// console.log(str.includes('darsh'))
+
+
+// slice
+// console.log(str.slice(1,15))
+// console.log(str.slice(4))
+// console.log(str.slice(-15,-1))
+
+
+// const str = `Hello I'm Adarsh Gosavi...!`;
+// console.log(str.substring(2,9))
+
+
+// const a = "Adarsh"
+// const b = "Gosavi"
+// const c = a+"-->"+b
+
+// console.log(c)
+// console.log(15+"Adarsh")
+// console.log(15+987+"Gosavi"+50)
+
+
+
+// const str = `Hello I'm Adarsh Gosavi...!`;
+// console.log(str.replaceAll("ello","Sir"))
+
+
+
+// const user = `    Adarsh     Gosavi    `;
+// console.log(user.trim())
+// console.log(user.trimStart())
+// console.log(user.trimEnd())
+
+
+
+const name = "Adarsh,Rohan,Abhinav,Jay"
+console.log(name.split(","))
