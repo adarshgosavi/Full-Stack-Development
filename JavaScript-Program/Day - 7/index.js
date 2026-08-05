bba6f292ab48608cd976b5b93764f455d4f9eaf7
adarshgosavi <aadrshgosavi89@gmail.com>
2026-08-05 20:26:16 +0530

    Your commit message

---
+++ JavaScript-Program/Day - 7/index.js
@@ -0,0 +1,40 @@
+// Object
+// key value
+
+// const user = {
+//     name: "Adarsh",
+//     age: 20,
+//     emailId:"negi@gmail.com",
+//     amount:5000,
+//     "Plot no.": "83"
+// }
+
+// console.log(user["name"])
+// console.log(user.age)
+// console.log(user["age"])
+// console.log(user["Plot no."])
+
+// console.log(typeof user)
+//CRUD operation: Create read update delete
+// console.log(user.emailId)
+// user.aadhar = 1234;
+// user.amount = 6000
+
+// // delete user.emailId;
+// console.log(user)
+
+
+
+const user = {
+    name: "Adarsh",
+    age: 20,
+    emailId:"negi@gmail.com",
+    amount:5000,
+    "Plot no.": "83"
+}
+
+// const user2 = user
+// user2.age = 21
+// console.log(user)
+
+console.log(Object.keys(user))
