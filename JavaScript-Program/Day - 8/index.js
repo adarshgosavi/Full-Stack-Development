51646f4f3b9fa3705614c07e00be62b3cc211706
adarshgosavi <aadrshgosavi89@gmail.com>
2026-08-08 23:28:22 +0530

    Your commit message

--- JavaScript-Program/Day - 8/index.js
+++ JavaScript-Program/Day - 8/index.js
@@ -49,8 +49,73 @@
 
 
 // Function: Expression 
-const addNumber = function(num1,num2){
-    return num1 + num2;
+
+
+// console.log(addNumber(10,5))
+// function addNumber(num1, num2){
+//     return num1 + num2;
+// }
+
+
+// const addNumber = function(num1,num2){
+//     return num1 + num2;
+// }
+// console.log(addNumber(5,5))
+
+
+
+// Arrow Function 
+
+
+
+// let arr = [54,7,5,26,43]
+// arr.sort((a,b) => a-b);
+// console.log(arr)
+
+
+// const addNumber = (num1,num2) => num1 + num2
+// // if we have single parameter , no need of this()
+// const squreNumber = num => num*num //this is a function
+
+// console.log(addNumber(5,8))
+// console.log(squreNumber(6))
+
+
+
+// const greeting = () => {
+//     return {
+//         name:"Adarsh",
+//         age:20
+//     }   
+// }
+
+
+// const greeting = () => ({name:"Adarsh",age:20})
+// console.log(greeting())
+
+
+
+// IIFE
+
+// (function greeting(){
+//     console.log("Hello World")
+// })();
+
+// (()=>{
+//     console.log("Nice to meet you")
+// })();
+
+
+//
+function greet(){
+    console.log("Hello Wolrd")
+
 }
 
-console.log(addNumber(5,5))
+function meet(callback){
+    console.log("Nice to meet you")
+    callback()
+    console.log("I have finished meeting")
+}
+
+meet(greet);
